7afc6cd3d4e0c66e4651de25219c68a3e1cdcc14
Prasad Surase <prasadsurase@gmail.com>
2018-10-29 09:13:35 +0530

    Added waitgroup feature

--- main.go
+++ main.go
@@ -1,115 +1,132 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"os"
-	"path/filepath"
+  "encoding/json"
+  "fmt"
+  "io"
+  "io/ioutil"
+  "log"
+  "net/http"
+  "os"
+  "path/filepath"
+  "sync"
 
-	"github.com/dchest/uniuri"
+  "github.com/dchest/uniuri"
 )
 
+var urlsCount int
+
 func main() {
-	root := "/Users/prasad/Downloads/Takeout_2/Google_Photos/"
+  root := "/Users/prasad/Downloads/Takeout_2/Google_Photos/"
+  var wg sync.WaitGroup
 
-	files := []string{}
-	// filepath.Walk is the function which lists all the nested directories and the files in those directories.
-	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			log.Panicln("Something went wrong in: ", path)
-			log.Fatal(err)
-			return err
-		}
+  files := []string{}
+  // filepath.Walk is the function which lists all the nested directories and the files in those directories.
+  err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+    if err != nil {
+      log.Panicln("Something went wrong in: ", path)
+      log.Fatal(err)
+      return err
+    }
 
-		fi, err := os.Stat(path)
-		if err != nil {
-			fmt.Println(err)
-			log.Fatal(err)
-			return err
-		}
-		switch mode := fi.Mode(); {
-		case mode.IsDir():
-			// fmt.Println("File:", path)
-		case mode.IsRegular():
-			ext := filepath.Ext(path)
-			if ext == ".json" {
-				files = append(files, path)
-				go handleJSONFile(&path)
-			}
-		}
-		return nil
-	})
+    fi, err := os.Stat(path)
+    if err != nil {
+      fmt.Println(err)
+      log.Fatal(err)
+      return err
+    }
+    switch mode := fi.Mode(); {
+    case mode.IsDir():
+      // fmt.Println("File:", path)
+    case mode.IsRegular():
+      ext := filepath.Ext(path)
+      if ext == ".json" {
+        files = append(files, path)
+        wg.Add(1)
+        go handleJSONFile(&wg, path)
+      }
+    }
+    return nil
+  })
 
-	if err != nil {
-		fmt.Println("Unable to open directory")
-		log.Fatal(err)
-	}
-	fmt.Println("Files count:", len(files))
+  if err != nil {
+    fmt.Println("Unable to open directory")
+    log.Fatal(err)
+  }
+  wg.Wait()
+  fmt.Println("Files count:", len(files))
+  fmt.Println("Urls Retrieved:", urlsCount)
 }
 
 // parse the passed file and collect the urls.
-func handleJSONFile(path *string) error {
-	var data map[string]interface{}
-	ext := filepath.Ext(*path)
-	fmt.Println("File:", *path, ", Extension:", ext)
-	fileData, err := ioutil.ReadFile(*path)
-	if err != nil {
-		fmt.Println(err)
-		log.Fatal(err)
-		return err
-	}
-	json.Unmarshal(fileData, &data)
-	fmt.Println("************************** JSON data *************************")
-	fmt.Println(data)
-	go parseData(data)
-	return nil
+func handleJSONFile(wg *sync.WaitGroup, path string) error {
+  var data map[string]interface{}
+  var fileWG sync.WaitGroup
+  ext := filepath.Ext(path)
+  fmt.Println("File:", path, ", Extension:", ext)
+  fileData, err := ioutil.ReadFile(path)
+  if err != nil {
+    fmt.Println(err)
+    log.Fatal(err)
+    return err
+  }
+  defer wg.Done()
+  json.Unmarshal(fileData, &data)
+  fmt.Println("************************** JSON data *************************")
+  fmt.Println(data)
+  fileWG.Add(1)
+  go parseData(&fileWG, data)
+  fileWG.Wait()
+  return nil
 }
 
-func parseData(data map[string]interface{}) error {
-	if data["url"] != nil && data["url"] != "" {
-		fmt.Println("---------------------------==================---------------------------------")
-		url := data["url"].(string)
-		fmt.Println(url)
-		go saveFile(url)
-	}
-	return nil
+func parseData(fileWG *sync.WaitGroup, data map[string]interface{}) error {
+  var parseWG sync.WaitGroup
+  if data["url"] != nil && data["url"] != "" {
+    fmt.Println("---------------------------==================---------------------------------")
+    url := data["url"].(string)
+    fmt.Println(url)
+    parseWG.Add(1)
+    go saveFile(&parseWG, url)
+  }
+  fileWG.Done()
+  parseWG.Wait()
+  return nil
 }
 
-func saveFile(url string) error {
-	fmt.Println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-	fmt.Println("saveFile: ", url)
-	resp, err := http.Get(url)
-	fmt.Println("Response:", resp)
-	if err != nil {
-		fmt.Println("Error:", err)
-		log.Fatal(err)
-		return err
-	}
+func saveFile(parseWG *sync.WaitGroup, url string) error {
+  fmt.Println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+  fmt.Println("saveUrl: ", url)
+  resp, err := http.Get(url)
+  fmt.Println("Response:", resp)
+  if (err != nil) && (resp.StatusCode != 200) {
+    fmt.Println("Error:", err)
+    log.Fatal(err)
+    return err
+  }
 
-	fmt.Println("Resp:", resp)
-	defer resp.Body.Close()
+  fmt.Println("Resp:", resp)
+  defer resp.Body.Close()
+  defer parseWG.Done()
 
-	//open a file for writing
-	file, err := os.Create("/Users/prasad/images/" + uniuri.New() + ".jpg")
-	fmt.Println("Saved file:", file)
-	defer file.Close()
-	if err != nil {
-		fmt.Println("Error:", err)
-		log.Fatal(err)
-		return err
-	}
-	fmt.Println("New File:", file)
-	// Use io.Copy to just dump the response body to the file. This supports huge files
-	_, err = io.Copy(file, resp.Body)
-	if err != nil {
-		fmt.Println("Error:", err)
-		log.Fatal(err)
-		return err
-	}
-	fmt.Println("Saved file:", file)
-	return nil
+  //open a file for writing
+  file, err := os.Create("/Users/prasad/images/" + uniuri.New() + ".jpg")
+  // fmt.Println("Saved file:", *file)
+  defer file.Close()
+  if err != nil {
+    fmt.Println("Error:", err)
+    log.Fatal(err)
+    return err
+  }
+  fmt.Println("Created New File:", *file)
+  // Use io.Copy to just dump the response body to the file. This supports huge files
+  size, err := io.Copy(file, resp.Body)
+  if err != nil {
+    fmt.Println("Error:", err)
+    log.Fatal(err)
+    return err
+  }
+  urlsCount++
+  fmt.Println("Saved file:", *file, ", Size:", size)
+  return nil
 }
