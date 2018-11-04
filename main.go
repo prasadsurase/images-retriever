eabb2b2ede9a2540135ce49fa5deeef9d2dbe02b
Prasad Surase <prasadsurase@gmail.com>
2018-11-04 10:28:03 +0530

    Added sleep time for go routines to complete

--- main.go
+++ main.go
@@ -3,22 +3,26 @@ package main
 import (
   "encoding/json"
   "fmt"
+  "io"
   "io/ioutil"
   "log"
   "net/http"
   "os"
   "path/filepath"
+  "strings"
   "sync"
   "time"
 
   "github.com/dchest/uniuri"
 )
 
-var urlsCount int
+var imgUrlsCount int
+var videoUrlsCount int
+var failedRespUrlsCount int
 
 func main() {
-  // root := "/Users/prasad/Downloads/Takeout_2/Google_Photos/"
-  root := "/Users/prasad/Desktop/Lohagad"
+  root := "/Users/prasad/Downloads/Takeout/Google_Photos/"
+  // root := "/Users/prasad.surase/Desktop/Lohagad"
   var wg sync.WaitGroup
 
   files := []string{}
@@ -45,6 +49,7 @@ func main() {
         files = append(files, path)
         wg.Add(1)
         go handleJSONFile(&wg, path)
+        time.Sleep(500 * time.Millisecond)
       }
     }
     return nil
@@ -56,7 +61,9 @@ func main() {
   }
   wg.Wait()
   fmt.Println("Files count:", len(files))
-  fmt.Println("Urls Retrieved:", urlsCount)
+  fmt.Println("Urls Retrieved:", imgUrlsCount)
+  fmt.Println("Vides Urls:", videoUrlsCount)
+  fmt.Println("Failed Response Urls: ", failedRespUrlsCount)
 }
 
 // parse the passed file and collect the urls.
@@ -73,8 +80,8 @@ func handleJSONFile(wg *sync.WaitGroup, path string) error {
   }
   defer wg.Done()
   json.Unmarshal(fileData, &data)
-  fmt.Println("************************** JSON data *************************")
-  fmt.Println(data)
+  // fmt.Println("************************** JSON data *************************")
+  // fmt.Println(data)
   fileWG.Add(1)
   go parseData(&fileWG, data)
   fileWG.Wait()
@@ -84,61 +91,109 @@ func handleJSONFile(wg *sync.WaitGroup, path string) error {
 func parseData(fileWG *sync.WaitGroup, data map[string]interface{}) error {
   var parseWG sync.WaitGroup
   if data["url"] != nil && data["url"] != "" {
-    fmt.Println("---------------------------==================---------------------------------")
     url := data["url"].(string)
-    fmt.Println(url)
     parseWG.Add(1)
     go saveFile(&parseWG, url)
+    time.Sleep(600 * time.Millisecond)
   }
   fileWG.Done()
   parseWG.Wait()
   return nil
 }
 
+// func saveFile(parseWG *sync.WaitGroup, url string) error {
+// 	if strings.Contains(url, "video-downloads") {
+// 		fmt.Println("Video url: ", url)
+// 		return nil
+// 	}
+// 	tr := &http.Transport{
+// 		MaxIdleConns:       10,
+// 		IdleConnTimeout:    30 * time.Second,
+// 		DisableCompression: true,
+// 	}
+// 	client := &http.Client{Transport: tr}
+// 	// fmt.Println("Url: ", url)
+
+// 	resp, err := client.Get(url)
+// 	// fmt.Println("Resp:", resp)
+
+// 	if resp == nil {
+// 		fmt.Println("Response was nil for url:", url)
+// 		return nil
+// 	}
+
+// 	if (err != nil) || (resp.StatusCode != http.StatusOK) {
+// 		fmt.Println("Error:", err)
+// 		fmt.Println("Url:", url)
+// 		log.Fatal(err)
+// 		return nil
+// 	}
+
+// 	defer resp.Body.Close()
+// 	defer parseWG.Done()
+
+// 	data, err := ioutil.ReadAll(resp.Body)
+// 	err = ioutil.WriteFile("/Users/prasad/Desktop/golang_images/"+uniuri.New()+".jpg", data, os.FileMode(0777))
+// 	if err != nil {
+// 		fmt.Println("Error:", err)
+// 		log.Fatal(err)
+// 		return nil
+// 	}
+// 	urlsCount++
+// 	return nil
+// }
+
 func saveFile(parseWG *sync.WaitGroup, url string) error {
-  urlsCount++
-  fmt.Println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-  fmt.Println("saveUrl: ", url)
-  fmt.Println("start Http Get ``````````````````````````````")
+  if strings.Contains(url, "video-downloads") {
+    fmt.Println("Video url: ", url)
+    videoUrlsCount++
+    return nil
+  }
   tr := &http.Transport{
-    MaxIdleConns:       100,
+    MaxIdleConns:       10,
     IdleConnTimeout:    30 * time.Second,
     DisableCompression: true,
   }
   client := &http.Client{Transport: tr}
-  resp, err := client.Get(url)
-  fmt.Println("done Http Get ``````````````````````````````")
-  fmt.Println("Response:", resp)
-  // if (err != nil) && (resp.StatusCode != 200) {
-  // 	fmt.Println("Error:", err)
-  // 	log.Fatal(err)
-  // 	return err
-  // }
+  // fmt.Println("Url: ", url)
 
-  fmt.Println("Resp:", resp)
+  resp, err := client.Get(url)
+  // fmt.Println("Resp:", resp)
+
+  if resp == nil {
+    fmt.Println("Response was nil for url:", url)
+    failedRespUrlsCount++
+    return nil
+  }
+
+  if (err != nil) || (resp.StatusCode != http.StatusOK) {
+    fmt.Println("Error:", err)
+    fmt.Println("Url:", url)
+    log.Fatal(err)
+    return nil
+  }
+
   defer resp.Body.Close()
   defer parseWG.Done()
 
-  //open a file for writing
-  // file, err := os.Create("/Users/prasad/Desktop/golang_images/" + uniuri.New() + ".jpg")
+  // open file for writing
+  file, err := os.Create("/Users/prasad/Desktop/golang_images/" + uniuri.New() + ".jpg")
   // fmt.Println("Saved file:", *file)
-  // defer file.Close()
-  // if err != nil {
-  // 	fmt.Println("Error:", err)
-  // 	log.Fatal(err)
-  // 	return err
-  // }
-  // fmt.Println("Created New File:", *file)
-  // Use io.Copy to just dump the response body to the file. This supports huge files
-  // size, err := io.Copy(file, resp.Body)
-  data, err := ioutil.ReadAll(resp.Body)
-  err = ioutil.WriteFile("/Users/prasad/Desktop/golang_images/"+uniuri.New()+".jpg", data, os.FileMode(0777))
   if err != nil {
     fmt.Println("Error:", err)
     log.Fatal(err)
-    return err
+    return nil
   }
-  fmt.Println()
-  // fmt.Println("Saved file:", *file, ", Size:", size)
+  defer file.Close()
+
+  size, err := io.Copy(file, resp.Body)
+  if err != nil {
+    fmt.Println("Error:", err)
+    log.Fatal(err)
+    return nil
+  }
+  imgUrlsCount++
+  time.Sleep(50 * time.Millisecond)
+  fmt.Println("Saved file:", file.Name(), ", Size:", size)
   return nil
 }
